020a0522f438f44c49dc5d0c986ee1694f42216a
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2023-08-22 15:16:09 -0300

    6 - Crie a página de Formulário de uma Nova Categoria

---
+++ news/form.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import News, Categories, Users
+
+
+class FormCategories(forms.ModelForm):
+    name = forms.CharField(max_length=200, label="Nome")
+
+    class Meta:
+        model = Categories
+        fields = ["name"]
+
+
+class FormNews(forms.ModelForm):
+    title = forms.CharField(max_length=300, label="Título")
+    author = forms.ModelChoiceField(
+        queryset=Users.objects.all(), label="Autoria"
+    )
+    content = forms.CharField(widget=forms.Textarea, label="Conteúdo")
+    created_at = forms.DateField(
+        widget=forms.DateInput(attrs={"type": "date"}), label="Criado em"
+    )
+    image = forms.ImageField(label="URL da Imagem")
+
+    class Meta:
+        model = News
+        fields = ["title", "author", "content", "created_at", "image"]
